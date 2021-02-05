e913a186fb689098b6b26b918d5f0b99f3feb2e0
Sushmita297 <66064310+Sushmita297@users.noreply.github.com>
2021-02-05 09:07:57 +0000

    first commit

---
+++ demo.java
@@ -0,0 +1,10 @@
+import java.io.*; 
+  
+class demo { 
+    public static void main(String[] args) 
+    { 
+        System.out.println("Welcome"); 
+        System.out.println("To"); 
+        System.out.println("Simplilearn"); 
+    } 
+} 
